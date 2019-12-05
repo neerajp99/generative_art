df885510d00b5f7e8438704e98a61c3da08b2f98
neerajp99 <neerajp1999@gmail.com>
2019-12-05 23:41:08 +0530

    Add function to get points on the canvas

--- random_semi_circles/sketch.js
+++ random_semi_circles/sketch.js
@@ -1,12 +1,28 @@
-const canvasSketch = require('canvas-sketch');
+const canvasSketch = require("canvas-sketch");
 
 const settings = {
-  dimensions: [ 2048, 2048 ]
+  dimensions: [2048, 2048]
 };
 
 const sketch = () => {
+  const getPoints = () => {
+    const points = [];
+    const count = 40;
+    for (let i = 0; i < count; i++) {
+      for (let j = 0; j < count; j++) {
+        const u = i / (count - 1);
+        const v = j / (count - 1);
+
+        points.push({
+          position: [u, v]
+        });
+      }
+    }
+    return points;
+  };
+
   return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+    context.fillStyle = "white";
     context.fillRect(0, 0, width, height);
   };
 };
